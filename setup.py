cade13aa944752ab1af5b2db9176499b31808640
Allen Chiang <kuan-lun.chiang17@imperial.ac.uk>
2020-03-28 10:30:29 +0000

    package info init

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='time_series_transform',
+    version='v0',
+    packages=['time_series_transform',],
+    license='MIT',
+    long_description=open('README.md').read(),
+)
