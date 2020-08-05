279b9deb51c6d32d86b9d5456c65c0df869c3be1
chiang9 <jchiang1225@gmail.com>
2020-08-05 15:59:30 +0800

    test added and util arr check

--- time_series_transform/stock_transform/util.py
+++ time_series_transform/stock_transform/util.py
@@ -1,6 +1,10 @@
 from time_series_transform.transform_core_api.util import *
 
+def _arr_check(arr):
+    if len(arr) == 0:
+        raise ValueError("Data can not have zero length")
 
+    
 def macd(arr, return_diff = False):
     """
     Return the moving average convergence/divergence 
@@ -10,11 +14,15 @@ def macd(arr, return_diff = False):
     arr : array 
         data used to calculate the macd
 
+    return_diff : bool 
+        return difference between DIF and DEM if True
+
     Returns
     -------
     df : dict
         macd of the given data, including EMA_12, EMA_26, DIF, DEM, OSC
     """
+    _arr_check(arr)
     df = {}
     df['EMA_12'] = ema(arr, span=12).flatten()
     df['EMA_26'] = ema(arr, span=26).flatten()
@@ -41,6 +49,7 @@ def stochastic_oscillator(arr):
     ret : dict
         stochastic oscillator of the given data, including k and d values
     """
+    _arr_check(arr)
     rsv_day = 9
     ret = {}
     df = pd.DataFrame(arr)
@@ -48,8 +57,8 @@ def stochastic_oscillator(arr):
     rsv_rolling = df.rolling(rsv_day)
     rsv_val = 100*(df - rsv_rolling.min())/(rsv_rolling.max() - rsv_rolling.min())
     ret['k_val'] = rsv_val
-    ret['d_val'] = np.array(ret['k_val'].rolling(3).mean())
-    ret['k_val'] = np.array(rsv_val)
+    ret['d_val'] = np.array(ret['k_val'].rolling(3).mean()).reshape(-1)
+    ret['k_val'] = np.array(rsv_val).reshape(-1)
     
     return ret
 
@@ -67,6 +76,7 @@ def rsi(arr, n_day = 14):
     rsi : array
         Relative Strength Index of the given array
     """
+    _arr_check(arr)
     orgLen = len(arr)
     arr = arr[~np.isnan(arr)]
 
@@ -99,6 +109,7 @@ def williams_r(arr, n_day=14):
     r_val : array
         Relative Strength Index of the given array
     """
+    _arr_check(arr)
     orgLen = len(arr)
     arr = arr[~np.isnan(arr)]
     df = pd.DataFrame(arr)
--- time_series_transform/test/test_stock_base.py
+++ time_series_transform/test/test_stock_base.py
@@ -1,7 +1,16 @@
 import pytest
 from time_series_transform.stock_transform.base import *
 from time_series_transform.stock_transform.stock_extractor import *
+from time_series_transform.stock_transform.util import *
 
+# test marks
+#   - stock_extractor
+#   - portfolio_extractor
+#   - util
+#   - base 
+# Quck command to run mark: pytest -v -m <mark>
+
+@pytest.mark.stock_extractor
 @pytest.mark.parametrize("source", ["yahoo"])
 @pytest.mark.parametrize("stockSymbol", ["aapl", "0050.TW", "GC=F"])
 class Test_stock_extractor:
@@ -17,15 +26,123 @@ class Test_stock_extractor:
         data = se.get_stock_date(dates[0], dates[1])
         assert isinstance(data, Stock)
 
-@pytest.mark.parametrize("x", [5])
-class Test_stock:
-    @pytest.mark.parametrize("y", [5])
-    def test_cls(self, x,y):
-        assert x+y == 10
+@pytest.mark.portoflio_extractor
+@pytest.mark.parametrize("source", ["yahoo"])
+@pytest.mark.parametrize("stockList", [["aapl", "0050.TW", "GC=F"],[],["aapl"]])
+class Test_portfolio_extractor:
+    @pytest.mark.parametrize("periods", ["1y", "max", "1d"])
+    def test_extractor_get_portfolio_period(self, stockList, periods,source):
+        se = Portfolio_Extractor(stockList,source)
+        data = se.get_portfolio_period(periods)
+        assert isinstance(data, Portfolio)
 
-    class test_t:
-        @pytest.mark.parametrize("y", [9])
-        def test_clas(self, x,y):
-            assert x+y == 14
+    @pytest.mark.parametrize("dates", [["2019-01-01", "2020-07-01"], ["2015-01-01", "2020-01-01"],["2020-01-01", "2020-01-01"]])
+    def test_extractor_get_portfolio_date(self,stockList, dates, source):
+        pe = Portfolio_Extractor(stockList,source)
+        data = pe.get_portfolio_date(dates[0], dates[1])
+        assert isinstance(data, Portfolio)
 
 
+@pytest.mark.util
+class Test_stock_util:
+
+    @pytest.fixture(scope = 'class')
+    def arr(self):
+        se = Stock_Extractor('aapl','yahoo').get_stock_period('1y').df['Close']
+        data = list([[], [100], [100,20,30], se])
+        return data
+
+
+    @pytest.mark.parametrize("return_diff", [True,False])
+    def test_macd(self, arr, return_diff):
+        for ar in arr:
+            macd_res = []
+            oriLen = len(ar)
+            if oriLen == 0:
+                with pytest.raises(ValueError):
+                    macd_res = macd(ar, return_diff)
+            else:
+                macd_res = macd(ar, return_diff)
+                outKeys = []
+                
+                if return_diff:
+                    assert len(macd_res) == oriLen
+                else:
+                    col = list(macd_res.keys())
+                    outKeys = ['EMA_12', 'EMA_26', 'DIF', 'DEM', 'OSC']
+                    assert np.array_equal(col, outKeys)
+                    for key in outKeys:
+                        assert len(macd_res[key].shape) == 1
+                        assert macd_res[key].shape[0] == oriLen 
+
+
+    def test_stochastic_oscillator(self, arr):
+        for ar in arr:
+            oriLen = len(ar)
+            so_res = stochastic_oscillator(ar)
+            col = list(so_res.keys())
+            outKeys = ['k_val', 'd_val']
+            assert np.array_equal(col, outKeys)
+            for key in outKeys:
+                assert len(so_res[key].shape) == 1
+                assert so_res[key].shape[0] == oriLen
+
+    def test_rsi(self, arr):
+        for ar in arr:
+            oriLen = len(ar)
+            rsi_res = rsi(ar)
+            assert len(rsi_res.shape) == 1
+            assert rsi_res.shape[0] == oriLen
+
+    def test_williams_r(self, arr):
+        for ar in arr:
+            oriLen = len(ar)
+            w_res = williams_r(ar)
+            assert len(w_res.shape) == 1
+            assert w_res.shape[0] == oriLen
+
+@pytest.mark.base
+class Test_base:
+    def test_stock_make_technical_indicator(self,stock_test_sample):
+        pass
+
+    def test_portfolio_make_technical_indicator(self,portfolio_test_sample):
+        pass
+
+    def test_portfolio_get_portfolio_dataFrame(self,portfolio_test_sample):
+        pass
+
+    def test_portfolio_remove_different_date(self,portfolio_test_sample):
+        pass
+
+def sel_sort(data):
+
+  if not isinstance(data, list):
+      vals = list(data)
+  else:
+      vals = data
+
+  size = len(vals)
+
+  for i in range(0, size):
+
+      for j in range(i+1, size):
+
+          if vals[j] < vals[i]:
+              _min = vals[j]
+              vals[j] = vals[i]
+              vals[i] = _min
+  return vals
+
+
+@pytest.fixture
+def data():
+
+    return [[3, 2, 1, 5, -3, 2, 0, -2, 11, 9],[3, 2, 1, 5, -3, 2, 0, -2, 11, 9]]
+
+@pytest.mark.omg
+# @pytest.mark.parametrize("arr", data)
+def test_sel_sort(data):
+    for i in data:
+        sorted_vals = sel_sort([3, 2, 1, 5, -3, 2, 0, -2, 11, 9])
+        assert sorted_vals == sorted(i)
