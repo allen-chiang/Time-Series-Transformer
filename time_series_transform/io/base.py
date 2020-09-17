09e27684dbd7416b0fdd9cbe36bd029232018344
Allen Chiang <kuanlun.chiang@outlook.com>
2020-09-17 08:57:53 +0100

    cllection

--- time_series_transform/io/base.py
+++ time_series_transform/io/base.py
@@ -17,8 +17,34 @@ class io_base (object):
     def from_collection(self):
         pass
 
-    def to_collection(self):
-        pass
+    def to_collection(self,expandCategory,expandTimeIx,remove):
+        transCollection = copy.deepcopy(self.time_series)
+        if remove:
+            transCollection = transCollection.remove_different_time_index()
+        else:
+            transCollection = transCollection.pad_time_index()
+        if expandCategory:
+            transCollection = self._expand_dict_category(transCollection)
+        if expandTimeIx:
+            transCollection = self._expand_dict_date(transCollection)
+        resList = []
+        for i in transCollection:
+            if isinstance(transCollection[i],Time_Series_Data):
+                data = transCollection[i][:]
+                for j in data:
+                    data[j] = data[j].tolist()
+            else:
+                data = transCollection[i]
+            resList.append(data)
+        res = {}
+        for i in resList:
+            for key in i:
+                if key not in res:
+                    res[key] = i[key]
+                else:
+                    res[key]+= i[key]
+        return res
+
 
     def to_single(self,expandTime):
         if expandTime:
@@ -61,61 +87,3 @@ class io_base (object):
             dct[k] = tmp
         return dct
 
-
-
-
-
-    # def _expand_dict_category(self,collectionDict):
-    #     time_series = Time_Series_Data()
-    #     for i in collectionDict:
-    #         tmp =collectionDict[i]
-    #         tmp.sort()
-    #         for t in tmp.time_index:
-    #             time_series.set_time_index(tmp.time_index[t],t)
-    #         for d in tmp.data:
-    #             time_series.set_data(tmp.data[d],f'{d}_{i}')
-    #         for l in tmp.labels:
-    #             time_series.set_labels(tmp.labels[l],f'{l}_{i}')
-            
-    #     return {'1':time_series}
-
-    # def _expand_dict_date(self, collectionDict):
-    #     dct = {}
-    #     for k in collectionDict:
-    #         tmp = {}
-    #         a = collectionDict[k]
-    #         for i in range(a.time_length):
-    #             timeIx = list(a.time_index.keys())[0]
-    #             for t in a[i]:
-    #                 if t in a.time_index:
-    #                     continue
-    #                 if not isinstance(a[i][t], list) or not isinstance(a[i][t], np.ndarray):
-    #                     tmp[f"{t}_{a[i][timeIx]}"]=[a[i][t]]
-    #                 else:
-    #                     tmp[f"{t}_{a[i][timeIx]}"]=a[i][t]
-    #         dct[k] = tmp
-    #     return dct
-
-
-    # def make_dataframe(self, expandCategory, expandTimeIx):
-    #     resDf = pd.DataFrame()
-    #     transCollection = copy.copy(self.time_series_data_collection)
-    #     if expandCategory:
-    #         transCollection = self._expand_dict_category(transCollection)
-    #     if expandTimeIx:
-    #         transCollection = self._expand_dict_date(transCollection)
-    #     for i in transCollection:
-    #         if expandTimeIx is False:
-    #             data = transCollection[i][:]
-    #             for j in data:
-    #                 data[j] = data[j].tolist()
-    #             tmp = pd.DataFrame(data)
-    #         else:
-    #             data = transCollection[i]
-    #             for j in data:
-    #                 data[j] = data[j].tolist()
-    #             tmp = pd.DataFrame(data)
-    #         if expandCategory is False:
-    #             tmp[self._categoryIx] = i
-    #         resDf = resDf.append(tmp)
-    #     return resDf
