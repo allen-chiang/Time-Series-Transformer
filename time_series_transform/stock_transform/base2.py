11c68c623bb3b9c3c1c1da402a89aa9007dca785
chiang9 <jchiang1225@gmail.com>
2020-09-17 14:32:47 +0800

    base update

--- time_series_transform/stock_transform/base2.py
+++ time_series_transform/stock_transform/base2.py
@@ -10,4 +10,51 @@ from time_series_transform.transform_core_api.util import *
 from time_series_transform.transform_core_api.base import *
 from time_series_transform.io import *
 
-class Stock()
+class Stock(Time_Series_Data):
+    def __init__(self, symbol, data,additionalInfo = None, timeSeriesCol='Date'):
+        super().__init__()
+        self.symbol = symbol
+        self.additionalInfo = additionalInfo
+        self.timeSeriesCol = timeSeriesCol
+        
+    def get_dataFrame(self):
+        pass
+
+    def plot(self, colName = 'Close', *args, **kwargs):
+        pass
+
+    def make_technical_indicator(self, colName, labelName, indicatorFunctions, *args, **kwargs):
+        pass
+
+class Portfolio(Time_Series_Data_Colleciton):
+    def __init__(self, stockList, timeSeriesCol = 'Date', categoryCol = 'symbol'):
+        
+        super().__init__(self,time_series_data, timeSeriesCol, categoryCol)
+
+    def make_technical_indicator(self,colName, LabelName, indicator, n_job =1, verbose = 0, *args, **kwargs):
+        pass
+
+    def get_portfolio_dataFrame(self):
+        pass
+
+    def weight_calculate(self,weights={}, colName = 'Close'):
+        pass
+
+    def plot(self,stockIndicators, keyCol = 'Default' ,samePlot=False,*args,**kwargs):
+        """
+        plot the given stock indicator in the portfolio
+        
+        Parameters
+        ----------
+        stockIndicators : dict 
+            dictionary of the stock indicator used for plotting 
+        keyCol : str
+            xaxis of the plot, by default "Default"
+            valid value: columns of the data
+        samePlot : boolean
+            whether to show the stock in the same plot
+        """
+        pass
+
+
+    
