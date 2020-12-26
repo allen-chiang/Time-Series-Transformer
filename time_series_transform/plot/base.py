8204845d4b5d6f22f98dbaebad485602f94474a5
chiang9 <jchiang1225@gmail.com>
2020-12-25 21:26:05 -0500

    marker and plot collection

--- time_series_transform/plot/base.py
+++ time_series_transform/plot/base.py
@@ -7,37 +7,56 @@ import plotly.graph_objects as go
 
 class plot_base(object):
     def __init__(self, time_series):
-        if isinstance(time_series, Time_Series_Data):
+        if isinstance(time_series, Time_Series_Data_Collection):
+            self.category = list(time_series.time_series_data_collection.keys())
+            self.time_series = time_series
+            self.time_index_data = time_series[self.category[0]].time_index[time_series[self.category[0]].time_seriesIx]
+            self.fig = go.Figure()
+            self.is_collection = True
+        elif isinstance(time_series, Time_Series_Data):
             self.time_series = time_series
             self.data = time_series[:]
             self.time_index_data = time_series.time_index[time_series.time_seriesIx]
             self.fig = go.Figure()
+            self.is_collection = False
         else:
-            raise ValueError("Input data is not Time_Series_Data")
+            raise ValueError("Input data must be Time_Series_Data")
 
     def add_line(self, col, lineType, showlegend = True, color = 'default'):
         fig = self.fig
-        data = self.data[col]
         time_indx = self.time_index_data
+        if self.is_collection:
+            for cat in self.category:
+                data = self.time_series[cat].data[col]
+                fig_data = (dict(type = lineType.lower(),
+                                            x = time_indx, 
+                                            y = data, 
+                                            mode= 'lines', 
+                                            name = cat+"_"+col,
+                                            showlegend=showlegend))
+                fig.add_trace(fig_data)
 
-        if color == 'default':
-            fig_data = (dict(type = lineType.lower(),
-                        x = time_indx, 
-                        y = data, 
-                        mode= 'lines', 
-                        name = col,
-                        showlegend=showlegend))
         else:
-            fig_data = (dict(type = lineType.lower(),
-                        x = time_indx, 
-                        y = data, 
-                        mode= 'lines', 
-                        name = col,
-                        showlegend=showlegend,
-                        line = dict(color = color)))
+            data = self.data[col]
 
-        
-        fig.add_trace(fig_data)
+            if color == 'default':
+                fig_data = (dict(type = lineType.lower(),
+                            x = time_indx, 
+                            y = data, 
+                            mode= 'lines', 
+                            name = col,
+                            showlegend=showlegend))
+            else:
+                fig_data = (dict(type = lineType.lower(),
+                            x = time_indx, 
+                            y = data, 
+                            mode= 'lines', 
+                            name = col,
+                            showlegend=showlegend,
+                            line = dict(color = color)))
+
+            
+            fig.add_trace(fig_data)
 
     def update_layout(self, **kwargs):
         self.fig.update_layout(**kwargs)
--- time_series_transform/plot/time_series_plot.py
+++ time_series_transform/plot/time_series_plot.py
@@ -1,6 +1,7 @@
 import numpy as np
 from time_series_transform.plot.base import plot_base
 import plotly.graph_objects as go
+from copy import copy
 
 class TimeSeriesPlot(plot_base):
     def __init__(self, time_series_data):
@@ -10,13 +11,56 @@ class TimeSeriesPlot(plot_base):
         for col in dataCols:
             self.add_line(col, lineType)
         
-        self.update_layout(
-            title = title,
-            xaxis_title = self.time_series.time_seriesIx,
-            yaxis_title = "value",
-            legend_title = "Legend"
-        )
+        if self.is_collection:
+            buttonList = list()
+            visible_array = np.zeros(len(self.category))
+            buttonList.append(dict(label = 'All',
+                                        method = 'update',
+                                        args = [{'visible': visible_array==0},
+                                                {'title': 'All',
+                                                'showlegend':True}]))
+            for indx in range(len(self.category)):
+                col = self.category[indx]
+                va = copy(visible_array)
+                va[indx] = 1
+                buttonList.append(dict(label = col,
+                                        method = 'update',
+                                        args = [{'visible': va==1},
+                                                {'title': col,
+                                                'showlegend':True}]))
 
+
+            self.update_layout(
+                updatemenus=[go.layout.Updatemenu(
+                            active=0,
+                            buttons=buttonList
+                            )
+                        ]
+            )
+        else:
+            self.update_layout(
+                title = title,
+                xaxis_title = self.time_series.time_seriesIx,
+                yaxis_title = "value",
+                legend_title = "Legend"
+            )
+
+        return self
+    
+    def add_marker(self,x,y,color,legendName,showlegend=True,marker='circle'):
+        self.fig.add_trace(
+            go.Scatter(
+                mode='markers',
+                x=x,
+                y=y,
+                name=legendName,
+                marker=dict(
+                    color = color,
+                    symbol=marker
+                ),
+                showlegend=showlegend
+            )
+        )
         return self
 
     def __repr__(self):
