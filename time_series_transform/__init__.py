493c60f8b5110e70cec0730b980fe2c1606bd197
Allen Chiang <49981062+allen-chiang@users.noreply.github.com>
2021-01-12 01:34:51 +0800

    resolve circular import error

--- time_series_transform/__init__.py
+++ time_series_transform/__init__.py
@@ -1,6 +1,5 @@
 from time_series_transform.transform_core_api.time_series_transformer import Time_Series_Transformer
 from time_series_transform.stock_transform.stock_transfromer import Stock_Transformer
-from time_series_transform import sklearn
 from time_series_transform.transform_core_api import tfDataset_adopter
 
 
@@ -8,5 +7,4 @@ __all__ = [
     "Time_Series_Transformer",
     "Stock_Transformer",
     "tfDataset_adopter",
-    "sklearn"
 ]
